f8042482c8625929e971ad81378eee97e3d9befa
ebsgit1 <134064666+ebsgit1@users.noreply.github.com>
2024-11-30 16:40:50 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://askareappi-final-1.onrender.com/",
     methods: ["GET", "POST", "PATCH"],
   })
 );
